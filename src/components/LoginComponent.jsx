2f3f6fb864b9b87ea848a788e2e94de490b02d32
erendoru <erendoru@hotmail.com>
2023-04-17 11:20:59 +0300

    firebase added, folder structure fixed.

---
+++ src/components/LoginComponent.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function LoginComponent() {
+  return (
+    <div>
+      <h1>Login Component</h1>
+    </div>
+  );
+}
---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDqD6yAlcFQamTeR3fOC6-ey-yS2-hVTlo",
+  authDomain: "linkedin-clone-42c83.firebaseapp.com",
+  projectId: "linkedin-clone-42c83",
+  storageBucket: "linkedin-clone-42c83.appspot.com",
+  messagingSenderId: "160818287393",
+  appId: "1:160818287393:web:2b41047bd56b6f2a2ea460",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
